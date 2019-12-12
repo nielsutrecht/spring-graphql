3aa758f37fb9acefd3f77f9bfebd7039fc8ba5c5
Niels Dommerholt <ndommerholt@gmail.com>
2019-12-12 06:57:36 +0100

    Initial commit

---
+++ src/main/kotlin/com/nibado/example/grapgql/GraphqlApplication.kt
@@ -0,0 +1,11 @@
+package com.nibado.example.grapgql
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GraphqlApplication
+
+fun main(args: Array<String>) {
+    runApplication<GraphqlApplication>(*args)
+}
---
+++ src/test/kotlin/com/nibado/example/grapgql/GraphqlApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nibado.example.grapgql
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GraphqlApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
